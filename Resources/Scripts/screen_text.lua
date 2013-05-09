8875cc8f09b15ec543209a906a6ee29edefbdd80
fy <fy0748@gmail.com>
2013-05-10 00:45:35 +0800

    修复回溯不改变文字颜色BUG，同时不再为每次的选择分支单独重复创建控件——by mter

--- Resources/Scripts/screen_text.lua
+++ Resources/Scripts/screen_text.lua
@@ -20,7 +20,7 @@ local TextColor={
 	SwitchCase=flux.Color(0.42,0.57,0.14),
 }
 
-local function SetNext(this,curpos,ch_info)
+local function SetNext(this,curpos)
 	local text = ScreenText.textlist[curpos]
 	if type(text) == 'string' then		
 		if ScreenText.t==nil then
@@ -68,9 +68,9 @@ local function SetNext(this,curpos,ch_info)
 		--设置立绘信息
 		if ScreenText.portrait then
 			ScreenText.portrait:AnimCancel()
-			ScreenText.portrait:SetSprite(ch_info[text[3]]):SetSize(8.8,20.34)--:SetAlpha(1)
+			ScreenText.portrait:SetSprite(ScreenText.ch_info[text[3]]):SetSize(8.8,20.34)--:SetAlpha(1)
 		else 
-			ScreenText.portrait = flux.View(this):SetSprite(ch_info[text[3]]):SetSize(8.8,20.34):SetHUD(true)--:SetAlpha(1)
+			ScreenText.portrait = flux.View(this):SetSprite(ScreenText.ch_info[text[3]]):SetSize(8.8,20.34):SetHUD(true)--:SetAlpha(1)
 			this:AddView(ScreenText.portrait,-2)
 		end
 		--立绘位置
@@ -89,26 +89,31 @@ local function SetNext(this,curpos,ch_info)
 		if type(text[6])=='table' then
 			if ScreenText.switchCase==nil then
 				ScreenText.switchCase={}
-			else
-				--下个对话存在分支,则隐藏上一个的分支
-				for i=1,#ScreenText.switchCase do
-					if ScreenText.switchCase[i] then
-						ScreenText.switchCase[i]:SetAlpha(0)
-					end
+				for i=1,3 do
+					--初始化三个选项,并隐藏
+					ScreenText.switchCase[i]=flux.TextView(this,nil,'wqy',''):SetTextColor(TextColor.SwitchCase)
+					ScreenText.switchCase[i]:SetPosition(-9,-5+-1*i):SetAlign(flux.ALIGN_LEFT):SetHUD(true):SetAlpha(0)
+					this:AddView(ScreenText.switchCase[i])
 				end
 			end
-			--添加分支
-			for i=1,#text[6] do
-				ScreenText.switchCase[i]=flux.TextView(this,nil,'wqy','>   '..text[6][i]):SetTextColor(TextColor.SwitchCase)
-				ScreenText.switchCase[i]:SetPosition(-9,-5+-1*i):SetAlign(flux.ALIGN_LEFT):SetHUD(true)
-				this:AddView(ScreenText.switchCase[i])
+
+			
+			for i=1,3 do
+				if text[6][i] then
+				--显示存在的选项
+					ScreenText.switchCase[i]:SetText('>   '..text[6][i]):SetAlpha(1)
+					ScreenText.switchNum=i
+				else
+				--隐藏不存在的选项
+					ScreenText.switchCase[i]:SetAlpha(0)
+				end
 			end
 			--显示选中条
 			if ScreenText.selection==nil then 
 				ScreenText.selection=flux.View(this):SetSize(1,1):SetSprite('Resources/Images/hand.png'):SetAlign(flux.ALIGN_LEFT):SetHUD(true)
 				this:AddView(ScreenText.selection)
 			end
-			ScreenText.selection:SetAlpha(1):SetPosition(-10.3,-5-ScreenText.curSelection)
+			ScreenText.selection:SetAlpha(1):SetPosition(-10.3,-5-1)
 			--设置存在分支
 			ScreenText.curExistSwitch=true
 			--callback
@@ -169,7 +174,7 @@ function ShowText(fromcode,textlist,ch_info,bgpic)
 
 		ScreenText.scr:lua_OnPush(wrap(function(this)
 			theWorld:PhyPause()
-			SetNext(this,1,ch_info)
+			SetNext(this,1)
 		end))
 
 		--恢复
@@ -199,13 +204,13 @@ function ShowText(fromcode,textlist,ch_info,bgpic)
 						this:SetFromCode(ScreenText.fromcode)
 						theWorld:PopScreen()
 					else
-						SetNext(this,ScreenText.curpos,ch_info)
+						SetNext(this,ScreenText.curpos)
 						--设置颜色
 						if ScreenText.readpos<=ScreenText.curpos then
 							ScreenText.readpos =ScreenText.curpos
-							ScreenText.t:SetColor(TextColor.UnfinshRead)
+							ScreenText.t:SetTextColor(TextColor.UnfinshRead)
 						else
-							ScreenText.t:SetColor(TextColor.FinishRead)
+							ScreenText.t:SetTextColor(TextColor.FinishRead)
 						end
 						ScreenText.curpos = ScreenText.curpos + 1
 					end
@@ -216,9 +221,9 @@ function ShowText(fromcode,textlist,ch_info,bgpic)
 					end
 					-- 这时候回溯前一句话
 					if ScreenText.curpos > 2 then
-						SetNext(this,ScreenText.curpos-2,ch_info)
+						SetNext(this,ScreenText.curpos-2)
 						--设置颜色
-						ScreenText.t:SetColor(TextColor.FinishRead)
+						ScreenText.t:SetTextColor(TextColor.FinishRead)
 						ScreenText.curpos = ScreenText.curpos - 1
 					end
 				elseif key == flux.GLFW_KEY_ESC then
@@ -230,14 +235,14 @@ function ShowText(fromcode,textlist,ch_info,bgpic)
 				if ScreenText.curExistSwitch then
 					if key==flux.GLFW_KEY_UP then
 						if ScreenText.curSelection <= 1 then
-							ScreenText.curSelection=#ScreenText.switchCase
+							ScreenText.curSelection=ScreenText.switchNum
 						else
 							ScreenText.curSelection=ScreenText.curSelection-1
 						end
 						
 						ScreenText.selection:SetPosition(-10.3,-5-ScreenText.curSelection)
 					elseif key==flux.GLFW_KEY_DOWN then
-						if ScreenText.curSelection>=#ScreenText.switchCase then
+						if ScreenText.curSelection>=ScreenText.switchNum then
 							ScreenText.curSelection=1
 							else
 							ScreenText.curSelection=ScreenText.curSelection+1
@@ -249,7 +254,10 @@ function ShowText(fromcode,textlist,ch_info,bgpic)
         end))
 
     end
-	
+    --当前选项分支数量
+    ScreenText.switchNum=1
+	--保存图片
+	ScreenText.ch_info=ch_info
 	--最后一个存在分支的位置
 	ScreenText.switchFlag=0
 	--当前是否存在分支选项
